0dc983960e2cc9558a6cf3606a54ad850b68b42f
shuangxunian <shuangxunian@gmail.com>
2025-01-26 17:50:09 +0800

    feat: 橡皮

--- src/components/option/tools-components/eraserTool.vue
+++ src/components/option/tools-components/eraserTool.vue
@@ -1,14 +1,41 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
+import App from '@/utils/App'
 
-onMounted(async () => {})
+const strokeWidth = ref<number>(1)
+
+watch(strokeWidth, (newWidth) => {
+  App.changeDisposition('strokeWidth', newWidth)
+})
+
+onMounted(() => {
+  App.changeDisposition('strokeWidth', strokeWidth.value)
+})
 </script>
 
 <template>
-  <div class="eraser-tool">eraser-tool</div>
+  <div class="eraser-tool">
+    <div class="setting">
+      <p>橡皮粗细</p>
+      <div class="slider">
+        <el-slider v-model="strokeWidth" :min="1" :max="60" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
 .eraser-tool {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  .setting {
+    .slider {
+      margin: 0 10px;
+    }
+    .picker {
+      margin-top: 6px;
+    }
+  }
 }
 </style>
