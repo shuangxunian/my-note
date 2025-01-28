6b0a440b93568ea1708fd40774c10306a15eafab
shuangxunian <shuangxunian@gmail.com>
2025-01-28 17:01:54 +0800

    feat: 登录页优化

--- src/views/login/UserLogin.vue
+++ src/views/login/UserLogin.vue
@@ -29,9 +29,13 @@ const rules = ref<FormRules>({
 })
 
 const loading = ref(false)
+const checkedAgree = ref(false)
 
 const handleLogin = async (formEl: FormInstance | undefined) => {
-  console.log(formEl?.validate)
+  if (!checkedAgree.value) {
+    ElMessage.error('请先同意用户协议')
+    return
+  }
   if (!formEl) return
 
   await formEl.validate((valid) => {
@@ -53,31 +57,53 @@ const handleLogin = async (formEl: FormInstance | undefined) => {
 <template>
   <div class="login-container">
     <div class="login-box">
-      <p class="title">我言</p>
-      <el-form ref="loginFormRef" :model="loginForm" :rules="rules" label-width="0" size="large">
-        <el-form-item prop="userMail">
-          <el-input v-model="loginForm.userMail" placeholder="邮箱" :prefix-icon="User" />
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            v-model="loginForm.password"
-            type="password"
-            placeholder="密码"
-            :prefix-icon="Lock"
-            show-password
-          />
-        </el-form-item>
-        <el-form-item>
-          <el-button
-            type="primary"
-            :loading="loading"
-            class="login-button"
-            @click="handleLogin(loginFormRef)"
-          >
-            登录
-          </el-button>
-        </el-form-item>
-      </el-form>
+      <div class="title-box">
+        <p class="title">我言</p>
+        <div class="title-line"></div>
+        <div class="title-line"></div>
+      </div>
+      <div class="form-box">
+        <el-form ref="loginFormRef" :model="loginForm" :rules="rules" label-width="0" size="large">
+          <el-form-item prop="userMail">
+            <div class="form-each-box">
+              <p class="text">邮箱：</p>
+              <div class="input">
+                <el-input v-model="loginForm.userMail" placeholder="请输入邮箱" />
+              </div>
+            </div>
+            <!-- <el-input v-model="loginForm.userMail" placeholder="邮箱" :prefix-icon="User" /> -->
+          </el-form-item>
+          <el-form-item prop="password">
+            <div class="form-each-box">
+              <p class="text">密码：</p>
+              <div class="input">
+                <el-input v-model="loginForm.password" type="password" placeholder="请输入密码" />
+                <el-link class="forget" :underline="false">忘记密码？</el-link>
+              </div>
+            </div>
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              :loading="loading"
+              class="login-button"
+              @click="handleLogin(loginFormRef)"
+            >
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+        <div class="check-agree">
+          <div class="check-agree-user">
+            <el-checkbox v-model="checkedAgree"></el-checkbox>
+            <span>我已阅读并同意<span class="user-agreement">用户协议</span></span>
+          </div>
+          <span>|</span>
+          <div>
+            <span class="user-agreement">注册用户</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -92,21 +118,78 @@ const handleLogin = async (formEl: FormInstance | undefined) => {
 
   .login-box {
     width: 400px;
+    height: 700px;
     padding: 40px;
     background: #fff;
     border-radius: 8px;
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 
-    .title {
-      font-size: 24px;
-      font-weight: bold;
-      text-align: center;
-      margin-bottom: 40px;
+    .title-box {
+      width: calc(100%-80px);
+      height: 70px;
+      border-radius: 8px;
+      border: 3px solid #999;
+      padding: 40px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .title {
+        font-size: 36px;
+        font-weight: bold;
+      }
+      .title-line {
+        border-bottom: 3px solid #000;
+      }
+    }
+    .form-box {
+      padding: 0 40px;
+      padding-top: 20px;
+      color: #333;
+      .form-each-box {
+        display: flex;
+        width: 100%;
+        .text {
+        }
+        .input {
+          flex: 1;
+          border-bottom: 1px solid #000;
+          display: flex;
+          .forget {
+            font-size: 12px;
+            width: 80px;
+          }
+        }
+      }
+      .check-agree {
+        display: flex;
+        font-size: 14px;
+        line-height: 32px;
+        justify-content: space-between;
+        .check-agree-user {
+          display: flex;
+          gap: 4px;
+        }
+        .user-agreement {
+          color: #409eff;
+          cursor: pointer;
+        }
+        .user-agreement:hover {
+          color: #7ebbff;
+        }
+      }
     }
 
     .login-button {
       width: 100%;
     }
   }
+}
+:deep(.el-input__wrapper) {
+  box-shadow: none !important;
+  // border-bottom: 1px solid black;
+  border-radius: 0;
 }
 </style>
